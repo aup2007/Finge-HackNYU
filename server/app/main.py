e6cb0678dd6f93d9f1b05331d165e7b754f37a06
ofargally <ofargally@colgate.edu>
2025-02-08 15:42:57 -0500

    git ignore updated

--- server/app/main.py
+++ server/app/main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
-from app.routers import some_router  # Import your routers here
 
 app = FastAPI()
 
-# Include routers
-app.include_router(some_router.router)
 
 @app.get("/")
 def read_root():
