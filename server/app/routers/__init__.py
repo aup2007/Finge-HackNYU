041026d708046ae6d882a722f554d38d7a6139f5
ofargally <ofargally@colgate.edu>
2025-02-08 15:14:29 -0500

    basic server

---
+++ server/app/routers/__init__.py
@@ -0,0 +1,16 @@
+fastapi-project
+├── app
+│   ├── __init__.py
+│   ├── main.py
+│   ├── models
+│   │   └── __init__.py
+│   ├── routers
+│   │   └── __init__.py
+│   └── schemas
+│       └── __init__.py
+├── tests
+│   ├── __init__.py
+│   └── test_main.py
+├── requirements.txt
+├── .env
+└── README.md
