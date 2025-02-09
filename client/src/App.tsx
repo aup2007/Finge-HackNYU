c76a39193c14a0037b7e83e2d4dcab4e5a9e4200
Faith Winford <141306476+fwinford@users.noreply.github.com>
2025-02-09 04:05:55 -0500

    private router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import LoginPage from "./pages/login.tsx";
 import StockTracker from "./pages/matches.tsx";
 import SelectionPage from "./pages/selection.tsx";
+import PrivateRoute from "./pages/PrivateRoute";
 
 export default function App() {
   const [isSignedUp, setIsSignedUp] = useState(false);
@@ -26,7 +27,9 @@ export default function App() {
           }
         />
         <Route path="/selection" element={<SelectionPage />} />
-        <Route path="/matches" element={<StockTracker />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/matches" element={<StockTracker />} />
+          </Route>
       </Routes>
     </AuthProvider>
   );
